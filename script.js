f100b9157ebc7ebe4595424600286c83789c0bb0
HaqtanEfe <84837235+HaqtanEfe@users.noreply.github.com>
2024-08-11 12:22:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add interactivity to your blog using JavaScript.
+// Example: Display an alert when the page is loaded.
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to My Blog!");
+});
